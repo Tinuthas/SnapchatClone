0fa02d73184cc93b3e310ea16a5b9db5470f3a83
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-02-15 15:38:53 -0300

    created project

---
+++ SnapchatClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnapchatClone
+//
+//  Created by Marcus Vinicius Galdino Medeiros on 15/02/20.
+//  Copyright © 2020 Marcus Vinicius Galdino Medeiros. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
